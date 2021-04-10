d55e52ea6df352d93e500d6ba0641401ee571602
kozlov anatolij <kozlov.aa@phystech.edu>
2021-04-10 21:29:56 +0300

    increase batch size

--- myscripts/convbert_cluster.py
+++ myscripts/convbert_cluster.py
@@ -53,14 +53,15 @@ def get_params(model_name, batch_size):
 
 
 def main():
-    training_args, train_dataset, model_dir, _ = get_params('convbert_4', 18)
+    training_args, train_dataset, model_dir, _ = get_params('convbert_4', 22)
     config = ConvbertConfig(hidden_size=768, num_attention_heads=12, intermediate_size=3072, attention_probs_dropout_prob=0, num_hidden_groups=4, num_hidden_layers=12, kernel_size=255)
     config.save_pretrained(model_dir)
     model = init_convbert_model(config)
     model.save_pretrained(model_dir)
 
     trainer = MyTrainer(model=model, args=training_args, train_dataset=train_dataset, prediction_loss_only=True)
-
+    print('kernel_size')
+    print(model.convbert.encoder.kernel_size)
     trainer.train(model_path=model_dir)
     trainer.save_model(model_dir)
 
