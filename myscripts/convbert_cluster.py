be0aa69a64a79fa21ea24d18a21304a443db2c85
kozlov anatolij <kozlov.aa@phystech.edu>
2021-04-14 18:58:10 +0300

    add continue arg

--- myscripts/convbert_cluster.py
+++ myscripts/convbert_cluster.py
@@ -3,6 +3,7 @@ from transformers import AlbertTokenizer, ConvbertForPreTraining, ConvbertConfig
 from transformers import AlbertForPreTraining, AlbertConfig
 from dataset import SOPDataset, MyTrainer
 import os
+import sys
 
 
 def get_last_checkpoint(dir_name):
@@ -17,8 +18,7 @@ def get_last_checkpoint(dir_name):
     return os.path.join(dir_name, result)
 
 
-def init_convbert_model(config):
-    model = ConvbertForPreTraining(config)
+def init_convbert_model(config, model):
     ready_model = AlbertForPreTraining.from_pretrained('albert-base-v2')
     model.convbert.set_input_embeddings(ready_model.albert.get_input_embeddings())
     return model
@@ -53,16 +53,27 @@ def get_params(model_name, batch_size):
 
 
 def main():
-    training_args, train_dataset, model_dir, _ = get_params('convbert_4', 18)
-    config = ConvbertConfig(hidden_size=768, num_attention_heads=12, intermediate_size=3072, attention_probs_dropout_prob=0, num_hidden_groups=4, num_hidden_layers=12, kernel_size=255)
-    config.save_pretrained(model_dir)
-    model = init_convbert_model(config)
+    training_args, train_dataset, model_dir, output_dir = get_params('convbert_4', 18)
+
+    if len(sys.argv) > 1:
+        checkpoint = get_last_checkpoint(output_dir)
+        model = ConvbertForPreTraining.from_pretrained(checkpoint)
+    else:
+        config = ConvbertConfig(hidden_size=768, num_attention_heads=12, intermediate_size=3072, attention_probs_dropout_prob=0, num_hidden_groups=4, num_hidden_layers=12, kernel_size=255)
+        config.save_pretrained(model_dir)
+        model = ConvbertForPreTraining(config)
+
+    model = init_convbert_model(config, model)
     model.save_pretrained(model_dir)
 
     trainer = MyTrainer(model=model, args=training_args, train_dataset=train_dataset, prediction_loss_only=True)
     print('kernel_size')
     print(model.convbert.encoder.kernel_size)
-    trainer.train(model_path=model_dir)
+    if len(sys.argv) > 1:
+        checkpoint = get_last_checkpoint(output_dir)
+        trainer.train(model_path=checkpoint)
+    else:
+        trainer.train(model_path=model_dir)
     trainer.save_model(model_dir)
 
 
