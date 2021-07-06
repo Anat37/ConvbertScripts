c0c98ee3ada37a667cd636b7e8a7abbbda71226a
kozlov anatolij <kozlov.aa@phystech.edu>
2021-07-06 22:30:37 +0300

    add jupyter notebooks

--- myscripts/continue_train_convbert.py
+++ myscripts/continue_train_convbert.py
@@ -2,25 +2,26 @@ from params import *
 
 
 def init_convbert_model(model):
-    ready_model = AlbertForPreTraining.from_pretrained('albert-base-v2')
-    model.convbert.set_input_embeddings(ready_model.albert.get_input_embeddings())
+    #ready_model = AlbertModel.from_pretrained('albert-base-v2')
+    #model.convbert.set_input_embeddings(ready_model.get_input_embeddings())
     return model
 
 def main():
-    training_args, train_dataset, model_dir, output_dir = get_params('convbert_2', 18)
+    training_args, train_dataset, model_dir, output_dir = get_params('convbert_12_127', 24)
 
-    checkpoint = get_last_checkpoint(output_dir)
+    #checkpoint = get_last_checkpoint(output_dir)
     #checkpoint = get_last_checkpoint('E:/ConvbertData/convbert_test/output')
     training_args.num_train_epochs = 4
     #training_args.learning_rate = 0.000506
 
-    model = ConvbertForPreTraining.from_pretrained(checkpoint)
+    model = ConvbertForPreTraining.from_pretrained(model_dir)
+    #model_embs = ConvbertModel.from_pretrained(model_dir)
     model = init_convbert_model(model)
-    model.save_pretrained(model_dir)
+    #model.save_pretrained(model_dir)
 
     trainer = MyTrainer(model=model, args=training_args, train_dataset=train_dataset, prediction_loss_only=True)
 
-    trainer.train(model_path=checkpoint)
+    trainer.train(model_path=model_dir)
     trainer.save_model(model_dir)
 
 
